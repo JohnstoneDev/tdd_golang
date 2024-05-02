e2d7d67bb981d21d3a8a35a6294a11bf30350b1f
Marucha Johnstone <marucha.webdev@gmail.com>
2024-05-02 18:20:56 +0300

    feat(arrays_slices): Add Benchmarks for tests, use reflect.DeepEqual for slice comparison

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -11,3 +11,20 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+// Function that sums the 'tails' of arrays
+// (items after the first element) in the collection
+func SumAllTails(numbersToSum ...[]int) []int{
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -2,6 +2,8 @@ package arraysslices
 
 import (
 	"fmt"
+	"reflect"
+	_"slices"
 	"testing"
 )
 func TestSum(t *testing.T) {
@@ -29,8 +31,54 @@ func TestSum(t *testing.T) {
 	})
 }
 
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		// You can use reflect.DeepEqual to check for the
+		// equality of the slices but it is unreliable
+		// slices.Equal should be used when the slices store the same type
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 1})
+		want := []int{5, 10}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
+
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]int{1, 2, 3, 4, 5, 56})
+		SumAllTails([]int{3, 4}, []int{10, 1})
+	}
+}
+
+func BenchmarkSumTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAllTails([]int{3, 4}, []int{10, 1})
+	}
+}
+
 func ExampleSum(){
 	sum := Sum([]int{1, 2, 3, 4, 5, 56})
 	fmt.Println(sum)
 	// Output : 71
+}
+
+
+func ExampleSumAllTails() {
+	got := SumAllTails([]int{3, 4}, []int{10, 1})
+	fmt.Println(got)
+	// Output : [1 1]
 }
